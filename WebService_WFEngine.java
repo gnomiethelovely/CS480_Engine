a5a21563f750d70ed73c6fa5bb5d99c63b4686a5
meilinyang <meilinyang42@gmail.com>
2015-05-18 15:43:07 -0700

    Added Webservice
    Will need more information for what to add to web service class

---
+++ WebService_WFEngine.java
@@ -0,0 +1,21 @@
+/*
+    Webservice for the workflow engine.
+    Calls webservice(to be defined later) and returns a value.
+
+    Gets called when Node wants to call service.
+*/
+
+public class WebService_WFEngine {
+    int value;
+    public WebService_WFEngine() {
+        /*
+        Define this class more later when we have more infomation for what
+        purpose this has.
+        */
+    }
+    
+    public int get_Value() {
+        return value;
+    }
+    
+}
