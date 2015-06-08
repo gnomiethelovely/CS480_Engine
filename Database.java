42e43a93bc9dfbc90511ee814542003cab6c4c5d
neilchoi <choi.neil@gmail.com>
2015-06-08 13:41:01 -0700

    update to main
    added a time variable to only check for new/updated objects in the
    database

--- Database.java
+++ Database.java
@@ -1,23 +1,20 @@
-package cs480.engine;
+/*
+    Database for the workflow engine.
+    Calls database and checks the state of the entry.
+
+*/
+
 
 public class Database {
     
-    public Database() {
-    }
-    
-    public void checkTimeStamp(/*JSON string*/) {
+    public void checkState() {
         //compare time from previous db list
         
-        if (Main.timeStamp < database:timeStamp) {
-            //get json string and put through decision method
-            Decision_WFEngine();
-        } else if (database:state==waiting for approval) {
+        if (database:state==waiting for approval) {
             die;
-        } else if (database:state==approved) {
-            // pass to where needed
+        } else (database:state==approved) {
+            // pass to where needed/Service
             Decision_WFEngine();
-        } else { //already complete
-            break;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 
-
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -8,33 +8,55 @@ import org.parse4j.*;
 import org.parse4j.callback.FindCallback;
 
 public class Main {
-    
+    static Date updatedAt = new Date(92, 1, 10);
+
     // run till terminated every 30 seconds
     public static void main(String[] args) {
+        WebService_WFEngine webService = new WebService_WFEngine();
+        Database database = new Database();
+        
+        
         ScheduledExecutorService executorService = 
                 Executors.newSingleThreadScheduledExecutor(); 
 
         Runnable task = new Runnable() {    
             public void run() {
                 try {
+                    
                     System.out.println("About to start querying database.");
                     String APP_ID = "ej29LXB9zHARKwcF5gHhkQ4SnJS7mGwWZ01qrZAa";
                     String API_ID = "qzclRmYPQZwd0guChjOdwFsSrIfvZXFCM42jXaIq";
                     
                     // Connect to parse.com database
                     Parse.initialize(APP_ID, API_ID);
-                    
-                    // Connect to Object
-                    ParseQuery<ParseObject> query = ParseQuery.getQuery("WorkflowUser");
+                    // Connect to form
+                    ParseQuery<ParseObject> query = 
+                            ParseQuery.getQuery("WorkflowUser");
                     
                     // Condition to search for
-                    //TODO: hopefully a key for if something is approved or nto
-                    query.whereEqualTo("Name", "FirstName");
+                    //TODO: hopefully a key for if something is approved/completed
+                    query.orderByAscending("UpdatedAt");
                     query.findInBackground(new FindCallback<ParseObject>() {
-                        public void done(List<ParseObject> results, ParseException e) {
-                            for (ParseObject a : results) {
-                                // Test for printing a value
-                                System.out.println(a.getString("email"));
+                        public void done(List<ParseObject> object, ParseException e) {
+                            for (ParseObject a : object) {
+                               if (updatedAt.compareTo(a.getUpdatedAt()) <= 0) {
+                                   
+                                   updatedAt = a.getUpdatedAt(); // keep track of time if no approval states
+                                   
+                                   
+                                   /* Send each object not approved/completed to webservice
+                                      for decision
+                                   */
+                                   webService.get_Value();
+                                   
+                                   // check state 
+                                   //database.checkState();
+                                   
+                                   // checking to see it calling from parse.com
+                                   System.out.println(a.getUpdatedAt());
+                               } else {
+                                   continue;
+                               }
                             }
                         }
                     });
